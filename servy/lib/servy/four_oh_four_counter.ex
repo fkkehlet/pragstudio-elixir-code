2e1ff8b140bfb7bf46e6b71aaf234fe56bc0b773
fkkehlet <developmedaddy@aliensz.org>
2024-06-14 13:41:05 +0200

    complete module 25

--- servy/lib/servy/four_oh_four_counter.ex
+++ servy/lib/servy/four_oh_four_counter.ex
@@ -1,57 +1,113 @@
-defmodule Servy.FourOhFourCounter do
-  @name __MODULE__
-
-  def start() do
-    pid = spawn(__MODULE__, :listen_loop, [%{}])
-    Process.register(pid, @name)
+defmodule Servy.FourOhGenServer do
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
     pid
   end
 
-  def bump_count(route) do
-    send(@name, {self(), :bump_count, route})
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
+    receive do {:response, response} -> response end
+  end
 
+  def cast(pid, message) do
+    send(pid, {:cast, message})
+  end
+
+  def listen_loop(state, callback_module) do
     receive do
-      {:response, status} -> status
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send(sender, {:response, new_state})
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts("Unexpected message: #{inspect(unexpected)}")
+        listen_loop(state, callback_module)
     end
+  end
+end
+
+defmodule Servy.FourOhFourCounter do
+  @name __MODULE__
+
+  alias Servy.GenericServer, as: FourOhGenServer
+
+  def start() do
+    FourOhGenServer.start(__MODULE__, %{}, @name)
+  end
+
+  def bump_count(route) do
+    FourOhGenServer.call(@name, {:bump_count, route})
   end
 
   def get_count(route) do
-    send(@name, {self(), :get_count, route})
-
-    receive do
-      {:response, count} -> count
-    end
+    FourOhGenServer.call(@name, {:get_count, route})
   end
 
   def get_counts() do
-    send(@name, {self(), :get_counts})
-
-    receive do
-      {:response, counts} -> counts
-    end
-
+    FourOhGenServer.call(@name, :get_counts)
   end
 
-  def listen_loop(state) do
-    receive do
-      {sender, :bump_count, route} ->
-        new_state = Map.update(state, route, 1, fn(current_count) -> current_count + 1 end)
-        send(sender, {:response, {:ok, "Bumped!"}})
-        listen_loop(new_state)
-
-      {sender, :get_count, route} ->
-        count = Map.get(state, route, "Route has not been hit!")
-        send(sender, {:response, count})
-        listen_loop(state)
-
-      {sender, :get_counts} ->
-        send(sender, {:response, state})
-        listen_loop(state)
-
-      unexpected ->
-        IO.puts("Unexpected message: #{inspect(unexpected)}")
-        listen_loop(state)
-    end
-
+  def reset() do
+    FourOhGenServer.cast(@name, :reset)
   end
+
+  def handle_call({:bump_count, route}, state) do
+    new_state = Map.update(state, route, 1, fn(current_count) -> current_count + 1 end)
+    {:ok, new_state}
+  end
+
+  def handle_call({:get_count, route}, state) do
+    count = Map.get(state, route, "Route has not been hit!")
+    {count, state}
+  end
+
+  def handle_call(:get_counts, state) do
+    {state, state}
+  end
+
+  def handle_cast(:reset, _state) do
+    %{}
+  end
+
+  # def listen_loop(state) do
+  #   receive do
+  #     {sender, :bump_count, route} ->
+  #       new_state = Map.update(state, route, 1, fn(current_count) -> current_count + 1 end)
+  #       send(sender, {:response, {:ok, "Bumped!"}})
+  #       listen_loop(new_state)
+  #
+  #     {sender, :get_count, route} ->
+  #       count = Map.get(state, route, "Route has not been hit!")
+  #       send(sender, {:response, count})
+  #       listen_loop(state)
+  #
+  #     {sender, :get_counts} ->
+  #       send(sender, {:response, state})
+  #       listen_loop(state)
+  #
+  #     unexpected ->
+  #       IO.puts("Unexpected message: #{inspect(unexpected)}")
+  #       listen_loop(state)
+  #   end
+  #
+  # end
 end
+
+alias Servy.FourOhFourCounter
+
+pid = FourOhFourCounter.start()
+
+send(pid, {:stop, "hammertime"})
+
+IO.inspect(FourOhFourCounter.bump_count("/larry"))
+IO.inspect(FourOhFourCounter.bump_count("/moe"))
+IO.inspect(FourOhFourCounter.bump_count("/curly"))
+IO.inspect(FourOhFourCounter.bump_count("/larry"))
+IO.inspect(FourOhFourCounter.get_count("/larry"))
+IO.inspect(FourOhFourCounter.get_count("/grace"))
+IO.inspect(FourOhFourCounter.get_counts())
+IO.inspect(Process.info(pid, :messages))
---
+++ servy/lib/servy/pledge_server_gen_server.ex
@@ -0,0 +1,102 @@
+defmodule Servy.GenericServer do
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
+    pid
+  end
+
+  # RPC, synchonous process which sends message to server and waits for a response
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
+    receive do {:response, response} -> response end
+  end
+
+  def cast(pid, message) do
+    send(pid, {:cast, message})
+  end
+
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send(sender, {:response, response})
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts("Unexpected message: #{inspect(unexpected)}")
+        listen_loop(state, callback_module)
+    end
+  end
+
+end
+
+defmodule Servy.PledgeServerGenServer do
+  # __MODULE__ ensures it is always unique, another option is to do @name :pledge_server
+  @name __MODULE__
+
+  alias Servy.GenericServer
+
+  # Client Interface
+
+  def start() do
+    IO.puts("Starting the pledge server...")
+    GenericServer.start(__MODULE__, [], @name)
+  end
+
+  def create_pledge(name, amount) do GenericServer.call(@name, {:create_pledge, name, amount}) end
+
+  def recent_pledges() do GenericServer.call(@name, :recent_pledges) end
+
+  def total_pledged() do GenericServer.call(@name, :total_pledged) end
+
+  def clear() do GenericServer.cast(@name, :clear) end
+
+  # Server callbacks
+
+  def handle_call(:total_pledged, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
+    {total, state}
+  end
+
+  def handle_call(:recent_pledges, state) do
+    {state, state}
+  end
+
+  def handle_call({:create_pledge, name, amount}, state) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+    most_recent_pledges = Enum.take(state, 2)
+    new_state = [{name, amount} | most_recent_pledges]
+    {id, new_state}
+  end
+
+  def handle_cast(:clear, _state) do
+    []
+  end
+
+  defp send_pledge_to_service(_name, _amount) do
+    # CODE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
+    {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
+end
+
+# # Client process code
+alias Servy.PledgeServerGenServer, as: GenServer
+
+# Server process is listen loop
+# pid = GenServer.start()
+#
+# send(pid, {:stop, "hammertime"})
+#
+# IO.inspect(GenServer.create_pledge("larry", 10))
+# IO.inspect(GenServer.create_pledge("moe", 20))
+# IO.inspect(GenServer.create_pledge("curly", 30))
+# IO.inspect(GenServer.create_pledge("daisy", 40))
+# IO.inspect(GenServer.create_pledge("grace", 50))
+#
+# IO.inspect(GenServer.recent_pledges())
+#
+# IO.inspect(GenServer.total_pledged())
+#
+# IO.inspect(Process.info(pid, :messages))
